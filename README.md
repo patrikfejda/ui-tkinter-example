7df9c77c8781a7b067f0383d5ec85c2ffafa02d3
Patrik Fejda <patrikfejda@gmail.com>
2023-11-23 12:42:14 +0100

    example

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ui-tkinter-example
+
+## Steps
+
+1. In command line run this code (windows) `pip.exe install pyqt5-tools`.Drag and drop the pip.exe into command line to get it's full path.
+
+2. Save UI file
+
+3. Run converty.py
---
+++ convert.py
@@ -0,0 +1,12 @@
+import os
+
+# Get a list of all .ui files in the current directory
+ui_files = [file for file in os.listdir() if file.endswith(".ui")]
+
+# Convert each .ui file to .py using pyuic5
+for ui_file in ui_files:
+    py_file = os.path.splitext(ui_file)[0] + ".py"
+    command = f"pyuic5 -x {ui_file} -o py/{py_file}"
+    os.system(command)
+
+print("Conversion completed.")

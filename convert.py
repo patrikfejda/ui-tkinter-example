e6e3813335d09456aafffd0b69d92e35194cd5c1
Patrik Fejda <patrikfejda@gmail.com>
2023-11-23 12:48:50 +0100

    move

--- convert.py
+++ convert.py
@@ -6,7 +6,7 @@ ui_files = [file for file in os.listdir() if file.endswith(".ui")]
 # Convert each .ui file to .py using pyuic5
 for ui_file in ui_files:
     py_file = os.path.splitext(ui_file)[0] + ".py"
-    command = f"pyuic5 -x {ui_file} -o py/{py_file}"
+    command = f"pyuic5 -x ui/{ui_file} -o py/{py_file}"
     os.system(command)
 
 print("Conversion completed.")
